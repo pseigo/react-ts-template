009563395dae0f2b276016969b9f4716b06a7fcc
Peyton Seigo <pseigo@yahoo.com>
2025-12-02 14:01:49 -0700

    [gw] Add 'isNaturalNumber' common util

---
+++ scripts/common/numbers.ts
@@ -0,0 +1,45 @@
+/*
+ * react-ts-template/scripts/common/numbers.ts
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2025 Peyton Seigo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+// TODO: extract all to tanaris
+
+import { isInteger } from "tanaris/numbers";
+
+/**
+ * Returns `true` iff `value` is a non-negative integer.
+ *
+ * @example isNaturalNumber(0); //=> true
+ * @example isNaturalNumber(1); //=> true
+ * @example isNaturalNumber(2n); //=> true
+ *
+ * @example isNaturalNumber(-1); //=> false
+ * @example isNaturalNumber(-2n); //=> false
+ * @example isNaturalNumber(2.5); //=> false
+ * @example isNaturalNumber(NaN); //=> false
+ * @example isNaturalNumber(Infinity); //=> false
+ */
+export function isNaturalNumber(value: unknown): value is number {
+  return isInteger(value) && (value as number) >= 0;
+}
